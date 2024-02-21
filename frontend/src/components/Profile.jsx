c52500dd9dd5ea619029b8babc5f9f81a83cc9bd
Yogithag0li <157909698+Yogithag0li@users.noreply.github.com>
2024-02-21 09:50:58 -0600

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import ProfileDetails from './ProfileDetails';
 
 function Profile() {
-  const [name, setName] = useState(localStorage.getItem('name') || 'Your Name');
-  const [description, setDescription] = useState(localStorage.getItem('description') || 'Your Biography');
+  const [name, setName] = useState(localStorage.getItem('name') || 'Name');
+  const [description, setDescription] = useState(localStorage.getItem('description') || 'Description');
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -20,42 +21,12 @@ function Profile() {
   return (
     <div className="container-fluid mt-4">
       <div className="row">
-        <div className="col-md-3">
-          {/* Profile Picture Container */}
-          <div className="profile-picture-container" style={{ width: '300px', height: '300px' }}>
-            <img
-              src="https://via.placeholder.com/300"
-              alt="Profile"
-              className="img-fluid"
-            />
-          </div>
-        </div>
-        <div className="col-md">
-          {/* Information Container */}
-          <div className="information-container">
-            <div className="form-group mb-3">
-              <input
-                type="text"
-                className="form-control border border-primary mt-3"
-                id="name"
-                placeholder="Enter name"
-                value={name}
-                onChange={handleNameChange}
-                style={{ maxWidth: '200px', width: '100%' }}
-              />
-            </div>
-            <div className="form-group">
-              <textarea
-                className="form-control border border-danger border-double"
-                id="description"
-                rows="7"
-                placeholder="Enter description"
-                value={description}
-                onChange={handleDescriptionChange}
-              ></textarea>
-            </div>
-          </div>
-        </div>
+        <ProfileDetails
+          name={name}
+          description={description}
+          handleNameChange={handleNameChange}
+          handleDescriptionChange={handleDescriptionChange}
+        />
       </div>
     </div>
   );
