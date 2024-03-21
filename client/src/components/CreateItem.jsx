53838228d9b705c34e281c01df1d2c32ad81e457
Yogithag0li <157909698+Yogithag0li@users.noreply.github.com>
2024-03-21 15:35:31 +0000

    Added nginx config

--- client/src/components/CreateItem.jsx
+++ client/src/components/CreateItem.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+
+const rootURL = process.env.ROOT_URL;
 
 const CreateItem = () => {
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState('');
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
   const [image, setImage] = useState(null);
 
   const handleNameChange = (e) => {
@@ -23,43 +25,61 @@ const CreateItem = () => {
 
     try {
       const formData = new FormData();
-      formData.append('name', name);
-      formData.append('quantity', quantity);
-      formData.append('image', image);
+      formData.append("name", name);
+      formData.append("quantity", quantity);
+      formData.append("image", image);
 
-      await axios.post('http://localhost:5000/create-item', formData, {
+      await axios.post(rootURL + "/create-item", formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+          "Content-Type": "multipart/form-data",
+        },
       });
       console.log(formData);
-      console.log();('Form submitted successfully');
+      console.log();
+      ("Form submitted successfully");
     } catch (error) {
-      console.error('Error submitting form:', error);
+      console.error("Error submitting form:", error);
     }
 
     // Reset form fields
-    setName('');
-    setQuantity('');
+    setName("");
+    setQuantity("");
     setImage(null);
   };
 
   return (
     <form onSubmit={handleSubmit} className="w-1/3 pt-4 pl-4">
       <h1 className="text-blue-700 font-bold pb-2">Add Grocery</h1>
-      <div className='pb-2'>
-        <label className='pr-2'>Name:</label>
-        <input type="text" className='outline rounded' value={name} onChange={handleNameChange} />
+      <div className="pb-2">
+        <label className="pr-2">Name:</label>
+        <input
+          type="text"
+          className="outline rounded"
+          value={name}
+          onChange={handleNameChange}
+        />
       </div>
-      <div className='pb-2'>
-        <label className='pr-2'>Quantity:</label>
-        <input type="number" className='outline rounded' value={quantity} onChange={handleQuantityChange} />
+      <div className="pb-2">
+        <label className="pr-2">Quantity:</label>
+        <input
+          type="number"
+          className="outline rounded"
+          value={quantity}
+          onChange={handleQuantityChange}
+        />
       </div>
-      <div className='pb-2'>
-        <label className='pr-2'>Image:</label>
-        <input type="file" name="image" accept="image/*" onChange={handleImageChange} />
+      <div className="pb-2">
+        <label className="pr-2">Image:</label>
+        <input
+          type="file"
+          name="image"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
       </div>
-      <button type="submit" className='bg-blue-500 p-1 px-2 rounded text-white'>Submit</button>
+      <button type="submit" className="bg-blue-500 p-1 px-2 rounded text-white">
+        Submit
+      </button>
     </form>
   );
 };
--- client/src/components/GetItems.jsx
+++ client/src/components/GetItems.jsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const rootURL = process.env.ROOT_URL;
 
 const GetItems = () => {
   const [items, setItems] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
-  const [name, setName] = useState('');
-  const [quantity, setQuantity] = useState('');
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/get-items');
+        const response = await axios.get(rootURL + "/get-items");
         setItems(response.data);
         if (response.data.length === 0) {
-            alert('No items in inventory');
-          }
+          alert("No items in inventory");
+        }
       } catch (error) {
-        console.error('Error fetching items:', error);
+        console.error("Error fetching items:", error);
       }
     };
 
@@ -31,13 +33,14 @@ const GetItems = () => {
 
   const handleDeleteClick = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/delete-item/${id}`);
+      await axios.delete(rootURL + `/delete-item/${id}`);
       // Refresh items after deletion
-      const response = await axios.get('http://localhost:5000/get-items');
+      const response = await axios.get(rootURL + "/get-items");
       setItems(response.data);
-      console.log();('Item deleted successfully');
+      console.log();
+      ("Item deleted successfully");
     } catch (error) {
-      console.error('Error deleting item:', error);
+      console.error("Error deleting item:", error);
     }
   };
 
@@ -53,66 +56,97 @@ const GetItems = () => {
     e.preventDefault();
 
     try {
-      await axios.patch(`http://localhost:5000/update-item/${selectedItem._id}`, {
+      await axios.patch(rootURL + `/update-item/${selectedItem._id}`, {
         name,
-        quantity
+        quantity,
       });
 
-      console.log('Item updated successfully');
+      console.log("Item updated successfully");
       setSelectedItem(null);
-      setName('');
-      setQuantity('');
+      setName("");
+      setQuantity("");
       // Refresh items after update
-      const response = await axios.get('http://localhost:5000/get-items');
+      const response = await axios.get(rootURL + "/get-items");
       setItems(response.data);
     } catch (error) {
-      console.error('Error updating item:', error);
+      console.error("Error updating item:", error);
     }
   };
 
   return (
-    <div className='w-3/4 pt-4'>
-      <h1 className='font-bold text-blue-700'>Groceries Inventory</h1>
+    <div className="w-3/4 pt-4">
+      <h1 className="font-bold text-blue-700">Groceries Inventory</h1>
       <table>
         <thead>
           <tr>
             <th>Image</th>
             <th>Name</th>
-            <th className='pr-3'>Quantity</th>
-            <th className='pr-3'>Update</th>
-            <th className='pr-3'>Delete</th>
+            <th className="pr-3">Quantity</th>
+            <th className="pr-3">Update</th>
+            <th className="pr-3">Delete</th>
           </tr>
         </thead>
         <tbody>
-          {Array.isArray(items) && items.map((item) => (
-            <tr key={item._id}>
-              <td className='pr-3'>
-                <img
-                  src={`http://localhost:5000/uploads/${item.image}`}
-                  alt={item.name}
-                  style={{ width: '150px', height: '150px' }}
-                />
-              </td>
-              <td className='pr-3'>{item.name}</td>
-              <td className='pr-3'>{item.quantity}</td>
-              <td className='pr-3'>
-                <button className='bg-green-500 p-1 px-2 rounded text-white' onClick={() => handleUpdateClick(item)}>Update</button>
-              </td>
-              <td className='pr-3'>
-                <button className='bg-red-500 p-1 px-2 rounded text-white' onClick={() => handleDeleteClick(item._id)}>Delete</button>
-              </td>
-              {selectedItem && selectedItem._id === item._id && (
-                <td className='pr-3'>
-                  <form onSubmit={handleSubmit}>
-                    <input type="text" className='outline rounded mr-2 mb-2' value={name} onChange={handleNameChange} />
-                    <input type="number" className='outline rounded mr-2'  value={quantity} onChange={handleQuantityChange} />
-                    <button type="submit" className='bg-blue-500 p-1 px-2 rounded text-white mr-2'>Submit</button>
-                    <button className='bg-red-500 p-1 px-2 rounded text-white' onClick={() => setSelectedItem(false)}>Cancel</button>
-                  </form>
+          {Array.isArray(items) &&
+            items.map((item) => (
+              <tr key={item._id}>
+                <td className="pr-3">
+                  <img
+                    src={rootURL + `/uploads/${item.image}`}
+                    alt={item.name}
+                    style={{ width: "150px", height: "150px" }}
+                  />
                 </td>
-              )}
-            </tr>
-          ))}
+                <td className="pr-3">{item.name}</td>
+                <td className="pr-3">{item.quantity}</td>
+                <td className="pr-3">
+                  <button
+                    className="bg-green-500 p-1 px-2 rounded text-white"
+                    onClick={() => handleUpdateClick(item)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td className="pr-3">
+                  <button
+                    className="bg-red-500 p-1 px-2 rounded text-white"
+                    onClick={() => handleDeleteClick(item._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+                {selectedItem && selectedItem._id === item._id && (
+                  <td className="pr-3">
+                    <form onSubmit={handleSubmit}>
+                      <input
+                        type="text"
+                        className="outline rounded mr-2 mb-2"
+                        value={name}
+                        onChange={handleNameChange}
+                      />
+                      <input
+                        type="number"
+                        className="outline rounded mr-2"
+                        value={quantity}
+                        onChange={handleQuantityChange}
+                      />
+                      <button
+                        type="submit"
+                        className="bg-blue-500 p-1 px-2 rounded text-white mr-2"
+                      >
+                        Submit
+                      </button>
+                      <button
+                        className="bg-red-500 p-1 px-2 rounded text-white"
+                        onClick={() => setSelectedItem(false)}
+                      >
+                        Cancel
+                      </button>
+                    </form>
+                  </td>
+                )}
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
