03fb6984d4345c87d5b29ae01078260732c3552b
Yogithag0li <157909698+Yogithag0li@users.noreply.github.com>
2024-03-21 15:45:08 +0000

    Fixed root url

--- client/src/components/CreateItem.jsx
+++ client/src/components/CreateItem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const rootURL = process.env.ROOT_URL;
+const rootURL = process.env.ROOT_URL || "";
 
 const CreateItem = () => {
   const [name, setName] = useState("");
--- client/src/components/GetItems.jsx
+++ client/src/components/GetItems.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const rootURL = process.env.ROOT_URL;
+const rootURL = process.env.ROOT_URL || "";
 
 const GetItems = () => {
   const [items, setItems] = useState([]);
